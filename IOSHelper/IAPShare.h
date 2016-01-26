d9e3d37986bf5ca0c0ca34bc7776bd517e850b0f
reallycsc <reallyc2@sina.com>
2016-01-26 15:33:34 +0800

    Squashed 'Classes/CSCClass/' content from commit 3b72aa6
    git-subtree-dir: Classes/CSCClass
    git-subtree-split: 3b72aa6c995563dd2a0eac9ec9e0f6552af1bd25

---
+++ IOSHelper/IAPShare.h
@@ -0,0 +1,30 @@
+//
+//  IAPShare.h
+//  ;
+//
+//  Created by Htain Lin Shwe on 10/7/12.
+//  Copyright (c) 2012 Edenpod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IAPHelper.h"
+
+#include "../CommonDefinition.h"
+#include "cocos2d.h"
+USING_NS_CC;
+using namespace std;
+
+@interface IAPShare : NSObject
+@property (nonatomic,strong) IAPHelper *iap;
+
++ (IAPShare *) sharedHelper;
+
+- (void)requestAllPurchasedProducts:(BOOL)production;
+
+- (void)requestPurchasedProductWithID:(NSString *)productID isProduction:(BOOL)production;
+
+- (void)buyProductWithID:(NSString *)productID;
+
++ (id)toJSON:(NSString*)json;
+
+@end
