46c99c6cfcace35b69ecd01d52b7350d5dbe67af
reallycsc <reallyc2@sina.com>
2016-02-05 18:02:12 +0800

    Try to fix git subtree push problem

--- Classes/CSCClass/CSCData/CSC_Encryption.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../CommonDefinition.h"
-
-char* csc_encode_base64(const string &str_to_encode);
-char* csc_decode_base64(const string &str_to_decode);
-string csc_encode_aes(const string& str_to_encode);
-string csc_decode_aes(const unsigned char* str_to_decode, const int &length);
